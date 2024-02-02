2cf57938048879f7fb51643eb65538d1dfead57c
Luiz Felipe <lffm1994@gmail.com>
2024-02-01 22:45:46 -0300

    Primeira rota com Gin

--- main.go
+++ main.go
@@ -2,7 +2,15 @@ package main
 
 import "github.com/gin-gonic/gin"
 
+func ExibeTodosAlunos(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"id":   "1",
+		"nome": "Gui Lima",
+	})
+}
+
 func main() {
 	r := gin.Default()
+	r.GET("/alunos", ExibeTodosAlunos)
 	r.Run()
 }
