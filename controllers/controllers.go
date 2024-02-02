ee26cabd64a6f94bfcd6c4f7ffb0befa9edc3344
Luiz Felipe <lffm1994@gmail.com>
2024-02-02 14:30:07 -0300

    Exibindo aluno por Id

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -31,3 +31,10 @@ func CriarNovoAluno(c *gin.Context) {
 	database.DB.Create(&aluno)
 	c.JSON(http.StatusOK, aluno)
 }
+
+func BuscaAlunoPorId(c *gin.Context) {
+	var aluno models.Aluno
+	id := c.Params.ByName("id")
+	database.DB.First(&aluno, id)
+	c.JSON(http.StatusOK, aluno)
+}
